8077268f4c456be1c3603e81bf3673506333bc2f
Matt.Wang <mattwang44@gmail.com>
2021-07-24 14:50:06 +0800

    fix: revive django debug toolbar in dev env

--- src/pycontw2016/settings/local.py
+++ src/pycontw2016/settings/local.py
@@ -29,6 +29,10 @@ MIDDLEWARE += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True if DEBUG else False,
+}
+
 # Install local, development apps.
 INSTALLED_APPS += env.tuple('LOCAL_APPS', default=())
 
